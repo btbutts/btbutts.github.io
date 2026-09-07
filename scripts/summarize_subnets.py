faf7fed152961a2106125fb098983128c5034b53
Brian T. Butts <30988627+btbutts@users.noreply.github.com>
2026-09-06 22:55:11 -0500

    modified:   resources/aws_subnets/global_aws_subnets.json 	modified:   scripts/summarize_subnets.py

--- scripts/summarize_subnets.py
+++ scripts/summarize_subnets.py
@@ -7,7 +7,7 @@ import json
 import sys
 from pathlib import Path
 
-MAX_LINES_PER_SEGMENT = 499
+MAX_LINES_PER_SEGMENT = 250
 
 def generate_output_paths(supplied_path: Path) -> tuple[Path, Path]:
     """Appends '_ipv4' and '_ipv6' to the file name stem of a given Path."""
